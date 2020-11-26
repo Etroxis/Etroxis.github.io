86c65f7c55ecb909e93d7ec492121daeec8f7f4c
Etroxis <59048123+Etroxis@users.noreply.github.com>
2020-11-26 09:31:57 +0100

    Neigung Test

--- scripts/app.js
+++ scripts/app.js
@@ -140,4 +140,17 @@ function intervalHandler(interval) {
   document.getElementById("moInterval").innerHTML = interval;
 }
 
+
+function ausrichtungAuslesen(ereignis) {
+  var winkelAlpha = Math.round(ereignis.alpha * 100) / 100; 
+  var winkelBeta = Math.round(ereignis.beta * 100) / 100;
+  var winkelGamma = Math.round(ereignis.gamma * 100) / 100;
+  document.getElementById('datenAlpha').innerHTML = winkelAlpha;
+  document.getElementById('datenBeta').innerHTML = winkelBeta;
+  document.getElementById('datenGamma').innerHTML = winkelGamma;
+  document.getElementById("kompass").style.webkitTransform = "rotate(" + winkelAlpha + "deg)";
+}
+window.onload = function() {
+window.addEventListener("deviceorientation", ausrichtungAuslesen, false);
+}
 }
