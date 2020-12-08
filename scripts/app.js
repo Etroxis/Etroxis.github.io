baf9b467c415306418e09daf3131e1f4b6b4c8bd
Etroxis <59048123+Etroxis@users.noreply.github.com>
2020-12-08 16:21:28 +0100

    Permissions added

--- scripts/app.js
+++ scripts/app.js
@@ -249,31 +249,71 @@ function persistentNotification() {
 }
 
 
+if ('permissions' in navigator) {
+  var logTarget = document.getElementById('logTarget');
 
-function scheduleNotification() {
-  if (!('Notification' in window)) {
-    alert('Notification API not supported');
-    return;
+  function handleChange(permissionName, newState) {
+    var timeBadge = new Date().toTimeString().split(' ')[0];
+    var newStateInfo = document.createElement('p');
+    newStateInfo.innerHTML = '' + timeBadge + ' State of ' + permissionName + ' permission status changed to ' + newState + '.';
+    logTarget.appendChild(newStateInfo);
   }
-  if (!('showTrigger' in Notification.prototype)) {
-    alert('Notification Trigger API not supported');
-    return;
+
+  function checkPermission(permissionName, descriptor) {
+    try {
+    navigator.permissions.query(Object.assign({name: permissionName}, descriptor))
+      .then(function (permission) {
+        document.getElementById(permissionName + '-status').innerHTML = permission.state;
+        permission.addEventListener('change', function (e) {
+          document.getElementById(permissionName + '-status').innerHTML = permission.state;
+          handleChange(permissionName, permission.state);
+        });
+      });
+    } catch (e) {
+    }
+  }
+
+  checkPermission('geolocation');
+  checkPermission('notifications');
+  checkPermission('push', {userVisibleOnly: true});
+  checkPermission('midi', {sysex: true});
+  checkPermission('camera');
+  checkPermission('microphone');
+  checkPermission('background-sync');
+  checkPermission('ambient-light-sensor');
+  checkPermission('accelerometer');
+  checkPermission('gyroscope');
+  checkPermission('magnetometer');
+
+  var noop = function () {};
+  navigator.getUserMedia = (navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia);
+  
+  function requestGeolocation() {
+    navigator.geolocation.getCurrentPosition(noop);
+  }
+
+  function requestNotifications() {
+    Notification.requestPermission();
+  }
+
+  function requestPush() {
+    navigator.serviceWorker.getRegistration()
+      .then(function (serviceWorkerRegistration) {
+        serviceWorkerRegistration.pushManager.subscribe();
+      });
+  }
+
+  function requestMidi() {
+    navigator.requestMIDIAccess({sysex: true});
   }
   
-  Notification.requestPermission()
-    .then(() => {
-      if (Notification.permission !== 'granted') {
-        throw 'Notification permission is not granted';
-      }
-    })
-    .then(() => navigator.serviceWorker.getRegistration())
-    .then((reg) => {
-      reg.showNotification("Hi there from the past!", {
-          showTrigger: new TimestampTrigger(new Date().getTime() + 10 * 1000)
-      })
-    })
-    .catch((err) => {
-      alert('Notification Trigger API error: ' + err);
-    });
+  function requestCamera() {
+    navigator.getUserMedia({video: true}, noop, noop)
   }
+  
+  function requestMicrophone() {
+    navigator.getUserMedia({audio: true}, noop, noop)
+  }
+}
+
 }
