eed390bd6e0a5dda7c941faf36498de377760a16
Etroxis <59048123+Etroxis@users.noreply.github.com>
2020-12-08 16:24:10 +0100

    No Neigung

--- scripts/app.js
+++ scripts/app.js
@@ -141,18 +141,18 @@ function intervalHandler(interval) {
 }
 
 
-function ausrichtungAuslesen(ereignis) {
-  var winkelAlpha = Math.round(ereignis.alpha * 100) / 100; 
-  var winkelBeta = Math.round(ereignis.beta * 100) / 100;
-  var winkelGamma = Math.round(ereignis.gamma * 100) / 100;
-  document.getElementById('datenAlpha').innerHTML = winkelAlpha;
-  document.getElementById('datenBeta').innerHTML = winkelBeta;
-  document.getElementById('datenGamma').innerHTML = winkelGamma;
-  document.getElementById("kompass").style.webkitTransform = "rotate(" + winkelAlpha + "deg)";
-}
-window.onload = function() {
-window.addEventListener("deviceorientation", ausrichtungAuslesen, false);
-}
+// function ausrichtungAuslesen(ereignis) {
+//   var winkelAlpha = Math.round(ereignis.alpha * 100) / 100; 
+//   var winkelBeta = Math.round(ereignis.beta * 100) / 100;
+//   var winkelGamma = Math.round(ereignis.gamma * 100) / 100;
+//   document.getElementById('datenAlpha').innerHTML = winkelAlpha;
+//   document.getElementById('datenBeta').innerHTML = winkelBeta;
+//   document.getElementById('datenGamma').innerHTML = winkelGamma;
+//   document.getElementById("kompass").style.webkitTransform = "rotate(" + winkelAlpha + "deg)";
+// }
+// window.onload = function() {
+// window.addEventListener("deviceorientation", ausrichtungAuslesen, false);
+// }
 
 
 if ('localStorage' in window || 'sessionStorage' in window) {
