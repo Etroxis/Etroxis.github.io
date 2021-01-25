abae05f8b5b7a2770f456f9324dbf60de8dbec99
Ikoko <iko89@hotmail.de>
2021-01-25 23:00:04 +0100

    storage

--- scripts/app.js
+++ scripts/app.js
@@ -187,51 +187,29 @@ function takePhoto() {
     .catch(err => alert('Error: ' + err));
 }
 
-//offline
-if ('localStorage' in window || 'sessionStorage' in window) {
-  var selectedEngine;
-
-  var logTarget = document.getElementById('target');
-  var valueInput = document.getElementById('value');
-
-  var reloadInputValue = function () {
-  console.log(selectedEngine, window[selectedEngine].getItem('myKey'))
-    valueInput.value = window[selectedEngine].getItem('myKey') || '';
-  }
-  
-  var selectEngine = function (engine) {
-    selectedEngine = engine;
-    reloadInputValue();
-  };
-
-  function handleChange(change) {
-    var timeBadge = new Date().toTimeString().split(' ')[0];
-    var newState = document.createElement('p');
-    newState.innerHTML = '' + timeBadge + ' ' + change + '.';
-    logTarget.appendChild(newState);
-  }
-
-  var radios = document.querySelectorAll('#selectEngine input');
-  for (var i = 0; i < radios.length; ++i) {
-    radios[i].addEventListener('change', function () {
-      selectEngine(this.value)
+//storage
+if ('storage' in navigator && 'estimate' in navigator.storage) {
+  navigator.storage.estimate()
+    .then(estimate => {
+      document.getElementById('usage').innerHTML = estimate.usage;
+      document.getElementById('quota').innerHTML = estimate.quota;
+      document.getElementById('percent').innerHTML = (estimate.usage * 100 / estimate.quota).toFixed(0);
     });
+}
+
+if ('storage' in navigator && 'persisted' in navigator.storage) {
+  navigator.storage.persisted()
+    .then(persisted => {
+      document.getElementById('persisted').innerHTML = persisted ? 'persisted' : 'not persisted';
+    });
+}
+
+function requestPersistence() {
+  if ('storage' in navigator && 'persist' in navigator.storage) {
+    navigator.storage.persist()
+      .then(persisted => {
+        document.getElementById('persisted').innerHTML = persisted ? 'persisted' : 'not persisted';
+      });
   }
-  
-  selectEngine('localStorage');
-
-  valueInput.addEventListener('keyup', function () {
-    window[selectedEngine].setItem('myKey', this.value);
-  });
-
-  var onStorageChanged = function (change) {
-    var engine = change.storageArea === window.localStorage ? 'localStorage' : 'sessionStorage';
-    handleChange('External change in ' + engine + ': key ' + change.key + ' changed from ' + change.oldValue + ' to ' + change.newValue + '');
-    if (engine === selectedEngine) {
-      reloadInputValue();
-    }
-  }
-
-  window.addEventListener('storage', onStorageChanged);
 }
   }
