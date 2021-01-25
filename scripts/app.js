3217a9492d00006dc101ca855865568412f8da95
Ikoko <iko89@hotmail.de>
2021-01-25 22:38:12 +0100

    style

--- scripts/app.js
+++ scripts/app.js
@@ -27,9 +27,37 @@ var watchId;
     }
   }
 
-  // geolocation
+  
+// pwa
 
-  var target = document.getElementById('target');
+var pwaCard = document.querySelector('#pwa');
+var pwaCardContent = pwaCard.querySelector('.card__content');
+var pwaCardDetails = pwaCard.querySelector('.card__details');
+var detailsShown = false;
+
+pwaCard.addEventListener('click', function (event) {
+  if (!detailsShown) {
+    detailsShown = true;
+    pwaCardContent.style.opacity = 0;
+    pwaCardDetails.style.display = 'block';
+    pwaCardContent.style.display = 'none';
+    setTimeout(function () {
+      pwaCardDetails.style.opacity = 1;
+    }, 300);
+  } else {
+    detailsShown = false;
+    pwaCardDetails.style.opacity = 0;
+    pwaCardContent.style.display = 'block';
+    pwaCardDetails.style.display = 'none';
+    setTimeout(function () {
+      pwaCardContent.style.opacity = 1;
+    }, 300);
+  }
+});
+
+// geolocation
+
+var target = document.getElementById('target');
 var watchId;
 
 function appendLocation(location, verb) {
@@ -158,30 +186,4 @@ function takePhoto() {
     })
     .catch(err => alert('Error: ' + err));
 }
-// pwa
-
-var pwaCard = document.querySelector('#pwa');
-var pwaCardContent = pwaCard.querySelector('.card__content');
-var pwaCardDetails = pwaCard.querySelector('.card__details');
-var detailsShown = false;
-
-pwaCard.addEventListener('click', function (event) {
-  if (!detailsShown) {
-    detailsShown = true;
-    pwaCardContent.style.opacity = 0;
-    pwaCardDetails.style.display = 'block';
-    pwaCardContent.style.display = 'none';
-    setTimeout(function () {
-      pwaCardDetails.style.opacity = 1;
-    }, 300);
-  } else {
-    detailsShown = false;
-    pwaCardDetails.style.opacity = 0;
-    pwaCardContent.style.display = 'block';
-    pwaCardDetails.style.display = 'none';
-    setTimeout(function () {
-      pwaCardContent.style.opacity = 1;
-    }, 300);
-  }
-});
   }
